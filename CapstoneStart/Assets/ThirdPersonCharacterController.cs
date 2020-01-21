65b3748a59d3aa86f3dface95cca0a50eadde135
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2020-01-20 18:18:09 -0600

    WASD Movement
    WASD currently works

---
+++ CapstoneStart/Assets/ThirdPersonCharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float Speed; //Character's move speed
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizantalMovemeneInput = Input.GetAxis("Horizontal"); //movement input for horizontal
+        float verticalMovemeneInput = Input.GetAxis("Vertical"); //movement input for vertical
+
+        Vector3 playerMovement = new Vector3(horizantalMovemeneInput, 0f, verticalMovemeneInput) * Speed * Time.deltaTime; //allows character to move across terrain
+        transform.Translate(playerMovement, Space.Self); //actually moves the character
+    }
+}
