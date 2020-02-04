b0aa0008d96ddceb0c3ab1f303637b1d4c4816c1
boydwNSU <47089310+boydwNSU@users.noreply.github.com>
2020-02-03 18:03:26 -0600

    Working on mechanics and holding gun
    Crouch still doesn't work right but I have an animator for armed and unarmed, just have to get the gun working on the armed animations

---
+++ CapstoneStartArmedAndUnarmedAnimators/Assets/Animations.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animations : MonoBehaviour
+{
+    public Animator manAnimator;
+    bool IsWalking = false;
+    bool StandingStill = false;
+    bool IsBackwardWalking = false;
+    bool IsRunning = false;
+    bool LeftWalk = false;
+    bool RightWalk = false;
+    //Testing bools, not meant to be final code
+    public bool HasRifle = false;
+    public bool CheckRightTurn = false;
+    public bool CheckLeftTurn = false;
+    public bool IsCrouch = false;
+    CapsuleCollider manCollider;
+    float MC_Height;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manAnimator =GetComponent<Animator>();//Get the animator
+
+        manCollider = GetComponent<CapsuleCollider>(); //Fetch the capsule collider
+
+        //Default collider size
+        MC_Height = 1.85f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manAnimator.SetBool("StandingStill", StandingStill);
+        manAnimator.SetBool("IsWalking", IsWalking);
+        manAnimator.SetBool("IsBackwardWalking", IsBackwardWalking);
+        manAnimator.SetBool("IsRunning", IsRunning);
+        manAnimator.SetBool("LeftWalk", LeftWalk);
+        manAnimator.SetBool("RightWalk", RightWalk);
+        //Test code, not meant to be final code
+        manAnimator.SetBool("HasRifle", HasRifle);
+        manAnimator.SetBool("CheckRightTurn", CheckRightTurn);
+        manAnimator.SetBool("CheckLeftTurn", CheckLeftTurn);
+        manAnimator.SetBool("IsCrouch", IsCrouch);
+
+        if (!Input.anyKey)
+        {
+            StandingStill = true;
+        }
+        if(Input.anyKey)
+        {
+            StandingStill = false;
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            IsWalking = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            IsWalking = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            IsBackwardWalking = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            IsBackwardWalking = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LeftWalk = true;
+        }
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            LeftWalk = false;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RightWalk = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            RightWalk = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            IsRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            IsRunning = false;
+        }
+        if(IsRunning == true)
+        {
+            IsWalking = false;
+        }
+        
+        //Test ifs for animations, not meant to be final code
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            HasRifle = true;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            CheckLeftTurn = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            CheckLeftTurn = false;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            CheckRightTurn = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            CheckRightTurn = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            IsCrouch = true;
+            IsWalking = false;
+            manCollider.height = 1.28f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            IsCrouch = false;
+            manCollider.height = MC_Height;
+            manCollider.direction = 1;
+        }
+
+
+
+    }
+}
---
+++ CapstoneStartArmedAndUnarmedAnimators/Assets/ThirdPersonCharacterController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public Collider manCollider;
+
+    public float Speed = 50f; //Character's move speed
+    public float RunSpeed = 200f; //Character speed while running
+    public float WalkSpeed = 50f; //Character speed while walking
+
+    // Update is called once per frame
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizantalMovemeneInput = Input.GetAxis("Horizontal"); //movement input for horizontal
+        float verticalMovemeneInput = Input.GetAxis("Vertical"); //movement input for vertical
+
+        Vector3 playerMovement = new Vector3(horizantalMovemeneInput, 0f, verticalMovemeneInput) * Speed * Time.deltaTime; //allows character to move across terrain
+        transform.Translate(playerMovement, Space.Self); //actually moves the character
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)) //If the left shift is held down
+        {
+            Speed = RunSpeed;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Speed = WalkSpeed;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+        
+    }
+
+    
+}
